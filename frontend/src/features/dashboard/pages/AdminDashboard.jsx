24b6b0822b2e7601e2098aa0655e1862dc438951
Sadiq Hussain <sa7434542@gmail.com>
2025-12-27 22:35:45 +0500

    solve issue left to right on refresh

--- frontend/src/features/dashboard/pages/AdminDashboard.jsx
+++ frontend/src/features/dashboard/pages/AdminDashboard.jsx
@@ -1,26 +1,23 @@
-import { useEffect } from "react";
-import i18n from "../../../i18n/index";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import i18n from "../../../i18n";
+
 import RecentOrdersTable from "../components/dashboardoverview/RecentCasesTable";
 import RevenueChart from "../components/dashboardoverview/CaseStageChart";
 import StatsCard from "../components/dashboardoverview/StatsCard";
 import OverviewHeader from "../components/dashboardoverview/OverviewHeader";
 
 const AdminDashboard = () => {
-  // Set the page direction based on selected language
-  useEffect(() => {
-    document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
-  }, [i18n.language]);
+  const { t } = useTranslation();
+  const isRTL = i18n.language === "ar";
 
   return (
     <div
-      dir={i18n.language === "ar" ? "rtl" : "ltr"}
-      className={`
-        min-h-screen
-        pt-16
-        px-3 sm:px-4 md:px-6
+      className={`min-h-screen pt-16
+        px-3 sm:px-4 md:px-6 lg:px-10
         py-3 sm:py-4 md:py-5
         transition-all duration-300
-        ${i18n.language === "ar" ? "lg:mr-[190px]" : "lg:ml-[190px]"}
+        ${isRTL ? "lg:mr-[190px] text-right" : "lg:ml-[190px] text-left"}
       `}
     >
       <OverviewHeader />
